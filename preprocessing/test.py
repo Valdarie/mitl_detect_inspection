4128872a61e2e313b15cc08bb972756f76872bed
Valerie <valerieleoenqi@gmail.com>
2024-11-15 02:48:36 +0800

    Horizontal Flip done

--- preprocessing/test.py
+++ preprocessing/test.py
@@ -17,26 +17,13 @@ from tqdm import tqdm
 
 coco_file_name  = "cassette_val"
 
-# Determine target split from coco_file_name
-target_split = None
-if "train" in coco_file_name.lower():
-    target_split = "train"
-elif "test" in coco_file_name.lower():
-    target_split = "test"
-elif "val" in coco_file_name.lower():
-    target_split = "val"
-if not target_split:
-    raise ValueError("Unable to determine target split from coco_file_name.")
-
-
-DATA_DIR = os.path.join("..", "testy")
+DATA_DIR = os.path.join(".", "testy") #. for script, .. for notebook
 COCO_DIR = os.path.join(DATA_DIR, "coco")
 
 ORG_ANNOTATION_PATH = os.path.join(DATA_DIR, "coco",target_split, f"{coco_file_name}_corrected_coco.json")
 SLC_ANNOTATION_PATH = os.path.join(DATA_DIR, "coco",target_split, f"{coco_file_name}_sliced_coco.json")
 
 AUGMENTATION_PATH = os.path.join(COCO_DIR, "augmentated") # Folder for all augmentations ./data/coco/augmentated
-
 
 #IMAGE_DIR = os.path.join(COCO_DIR, "images")
 IMAGE_DIR = os.path.join(COCO_DIR, "images")
@@ -54,69 +41,6 @@ os.path.exists(BBOX_VISUALIZATION_DIR)
 
 os.makedirs(BBOX_SAVE_DIR, exist_ok=True)
 os.makedirs(AUGMENTATION_PATH,exist_ok=True)
-
-coco_dict = load_json(ORG_ANNOTATION_PATH)
-[img.update({"file_name": img["file_name"].split("/")[-1]}) for img in coco_dict["images"]]
-save_json(coco_dict, save_path=ORG_ANNOTATION_PATH)
-
-for img in coco_dict["images"]:
-    fig, ax = plt.subplots(1, 1, figsize=(12, 9), constrained_layout=True)
-
-    mono_img = Image.open(os.path.join(IMAGE_DIR,img["file_name"])).convert("L")
-    rgb_img = Image.merge("RGB", (mono_img, mono_img, mono_img))
-
-    # iterate over all annotations
-    for ann_ind in range(len(coco_dict["annotations"])):
-        
-        if coco_dict["annotations"][ann_ind]["image_id"] == img["id"]:
-            # convert coco bbox to pil bbox
-            xywh = coco_dict["annotations"][ann_ind]["bbox"]
-            xyxy = [xywh[0], xywh[1], xywh[0]+xywh[2], xywh[1]+xywh[3]]
-
-            # visualize bbox over image
-            ImageDraw.Draw(rgb_img).rectangle(xyxy, width=5, outline="lime")
-
-    ax.axis("off")
-    ax.imshow(rgb_img)
-    fig.savefig(os.path.join(BBOX_SAVE_DIR, img["file_name"][:-4] + ".png")) #save each of the image original
-
-slc_dict= load_json(SLC_ANNOTATION_PATH)
-
-for img in slc_dict["images"]:
-    fig, ax = plt.subplots(1, 1, figsize=(12, 9), constrained_layout=True)
-    
-    # Open the sliced image file in grayscale, convert it to RGB
-    sliced_img_path = os.path.join(SLICED_IMAGE_DIR, img["file_name"])
-    mono_img = Image.open(sliced_img_path).convert("L")
-    rgb_img = Image.merge("RGB", (mono_img, mono_img, mono_img))
-    
-    # Iterate over all annotations for this specific image
-    for annotation in slc_dict["annotations"]:
-        if annotation["image_id"] == img["id"]:
-            # Extract and convert bounding box coordinates
-            xywh = annotation["bbox"]
-            xyxy = [xywh[0], xywh[1], xywh[0] + xywh[2], xywh[1] + xywh[3]]
-            
-            # Draw the bounding box on the image
-            ImageDraw.Draw(rgb_img).rectangle(xyxy, width=5, outline="lime")
-    
-    # Display and save the image with bounding boxes
-    ax.axis("off")
-    ax.imshow(rgb_img)
-    fig.savefig(os.path.join(BBOX_SAVE_DIR, img["file_name"][:-4] + ".png")) #save each of the image to folder called Each
-    plt.close()
-
-splits = ["train", "test", "val"]
-
-# Create 'original' and 'sliced' directories within each split
-for split in splits:
-    base_path = os.path.join(AUGMENTATION_PATH, split)
-    os.makedirs(base_path, exist_ok=True)
-    
-    # Create 'original' and 'sliced' subdirectories within each split
-    os.makedirs(os.path.join(base_path, "original"), exist_ok=True)
-    os.makedirs(os.path.join(base_path, "sliced"), exist_ok=True)
-
 
 def horizontal_flip_with_bboxes(
     image_dir,
@@ -182,6 +106,86 @@ def horizontal_flip_with_bboxes(
     save_json(coco_annotations, augmented_json_path)
     print(f"Augmented annotations saved to {augmented_json_path}")
 
-# Run the function for both original and sliced cases
-# horizontal_flip_with_bboxes(IMAGE_DIR, coco_dict, os.path.join(AUGMENTATION_PATH, target_split, "original", "horizontal_flip"), dataset_type="original")
-horizontal_flip_with_bboxes(SLICED_IMAGE_DIR, slc_dict, os.path.join(AUGMENTATION_PATH, target_split, "sliced", "horizontal_flip"), dataset_type="sliced")
+def run():
+    # Determine target split from coco_file_name
+    target_split = None
+    if "train" in coco_file_name.lower():
+        target_split = "train"
+    elif "test" in coco_file_name.lower():
+        target_split = "test"
+    elif "val" in coco_file_name.lower():
+        target_split = "val"
+    if not target_split:
+        raise ValueError("Unable to determine target split from coco_file_name.")
+
+    
+
+    coco_dict = load_json(ORG_ANNOTATION_PATH)
+    [img.update({"file_name": img["file_name"].split("/")[-1]}) for img in coco_dict["images"]]
+    save_json(coco_dict, save_path=ORG_ANNOTATION_PATH)
+
+    for img in coco_dict["images"]:
+        fig, ax = plt.subplots(1, 1, figsize=(12, 9), constrained_layout=True)
+
+        mono_img = Image.open(os.path.join(IMAGE_DIR,img["file_name"])).convert("L")
+        rgb_img = Image.merge("RGB", (mono_img, mono_img, mono_img))
+
+        # iterate over all annotations
+        for ann_ind in range(len(coco_dict["annotations"])):
+            
+            if coco_dict["annotations"][ann_ind]["image_id"] == img["id"]:
+                # convert coco bbox to pil bbox
+                xywh = coco_dict["annotations"][ann_ind]["bbox"]
+                xyxy = [xywh[0], xywh[1], xywh[0]+xywh[2], xywh[1]+xywh[3]]
+
+                # visualize bbox over image
+                ImageDraw.Draw(rgb_img).rectangle(xyxy, width=5, outline="lime")
+
+        ax.axis("off")
+        ax.imshow(rgb_img)
+        fig.savefig(os.path.join(BBOX_SAVE_DIR, img["file_name"][:-4] + ".png")) #save each of the image original
+
+    slc_dict= load_json(SLC_ANNOTATION_PATH)
+
+    for img in slc_dict["images"]:
+        fig, ax = plt.subplots(1, 1, figsize=(12, 9), constrained_layout=True)
+        
+        # Open the sliced image file in grayscale, convert it to RGB
+        sliced_img_path = os.path.join(SLICED_IMAGE_DIR, img["file_name"])
+        mono_img = Image.open(sliced_img_path).convert("L")
+        rgb_img = Image.merge("RGB", (mono_img, mono_img, mono_img))
+        
+        # Iterate over all annotations for this specific image
+        for annotation in slc_dict["annotations"]:
+            if annotation["image_id"] == img["id"]:
+                # Extract and convert bounding box coordinates
+                xywh = annotation["bbox"]
+                xyxy = [xywh[0], xywh[1], xywh[0] + xywh[2], xywh[1] + xywh[3]]
+                
+                # Draw the bounding box on the image
+                ImageDraw.Draw(rgb_img).rectangle(xyxy, width=5, outline="lime")
+        
+        # Display and save the image with bounding boxes
+        ax.axis("off")
+        ax.imshow(rgb_img)
+        fig.savefig(os.path.join(BBOX_SAVE_DIR, img["file_name"][:-4] + ".png")) #save each of the image to folder called Each
+        plt.close()
+
+    splits = ["train", "test", "val"]
+
+    # Create 'original' and 'sliced' directories within each split
+    for split in splits:
+        base_path = os.path.join(AUGMENTATION_PATH, split)
+        os.makedirs(base_path, exist_ok=True)
+        
+        # Create 'original' and 'sliced' subdirectories within each split
+        os.makedirs(os.path.join(base_path, "original"), exist_ok=True)
+        os.makedirs(os.path.join(base_path, "sliced"), exist_ok=True)
+
+    # Run the function for both original and sliced cases
+    # horizontal_flip_with_bboxes(IMAGE_DIR, coco_dict, os.path.join(AUGMENTATION_PATH, target_split, "original", "horizontal_flip"), dataset_type="original")
+    horizontal_flip_with_bboxes(SLICED_IMAGE_DIR, slc_dict, os.path.join(AUGMENTATION_PATH, target_split, "sliced", "horizontal_flip"), dataset_type="sliced")
+
+
+if __name__ == "__main__":
+    run()
